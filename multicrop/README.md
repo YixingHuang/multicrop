7efaee73de0d2c14fd340dab908c847948d7ea84
Yixing Huang <yixing.yh.huang@fau.de>
2023-09-11 14:39:01 +0200

    adjust folder
    adjust folder

--- multicrop/README.md
+++
@@ -1,9 +0,0 @@
-# How to install
-
-```
-git clone https://github.com/thuyen/multicrop.git
-
-cd multicrop
-
-python setup.py install
-```
--- multicrop/src/utils.h
+++
@@ -1,5 +0,0 @@
-#ifndef CUDA_1D_KERNEL_LOOP
-#define CUDA_1D_KERNEL_LOOP(i, n)                                 \
-  for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < (n); \
-       i += blockDim.x * gridDim.x)
-#endif
